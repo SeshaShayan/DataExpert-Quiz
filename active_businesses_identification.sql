e7b6d4aa0669d009916cbde4b8ef696f86073ab1
SeshaShayan <68672735+SeshaShayan@users.noreply.github.com>
2025-07-29 01:24:13 +1000

    Update active_businesses_identification.sql
    Changed logic. Used window function instead of CTEs

--- active_businesses_identification.sql
+++ active_businesses_identification.sql
@@ -1,21 +1,13 @@
- with occerence_avg_cte as
-  ( 
-      select 
-      event_type
-      , avg(occurences) as avg_occurences
-     
-      from playground.business_events
-     group by event_type
-),
-   
-   event_cnt_cte as(
-   
-     select be.business_id
-   , count(distinct be.event_type) as event_count
-     
-      from playground.business_events be inner join occerence_avg_cte oac
-     on be.event_type=oac.event_type and be.occurences>oac.avg_occurences
-     group by business_id
-   having  count(distinct be.event_type)>1
-)
-  select business_id from event_cnt_cte
+with windowed_occerences as (
+  SELECT business_id, event_type, occurences,
+  avg(occurences) over(partition by event_type)
+  as avg_event_occurence
+  FROM playground.business_events
+
+ )
+
+select business_id
+  from windowed_occerences
+  where occurences>avg_event_occurence
+group by business_id
+having count(distinct event_type)>1
