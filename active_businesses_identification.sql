c78d8956565c2cf8baad6edcc77b1331f610e435
SeshaShayan <68672735+SeshaShayan@users.noreply.github.com>
2025-07-28 21:18:24 +1000

    Update active_businesses_identification.sql
    Edited code to filter on conditions and added distinct count to deter from having to count duplicate event type for single business

--- active_businesses_identification.sql
+++ active_businesses_identification.sql
@@ -1,26 +1,21 @@
- with event_cnt_cte as(
-   
-     select business_id,count(event_type) as event_count --group by 1
-     -- LIMIT 100
-     
-      from playground.business_events
-     group by business_id
-   having count(event_type)>1
-),
-  occerence_avg_cte as
+ with occerence_avg_cte as
   ( 
       select 
       event_type
-      , avg(occurences) as avg_occurences --group by 1
-     -- LIMIT 100
+      , avg(occurences) as avg_occurences
      
       from playground.business_events
      group by event_type
+),
+   
+   event_cnt_cte as(
+   
+     select be.business_id
+   , count(distinct be.event_type) as event_count
+     
+      from playground.business_events be inner join occerence_avg_cte oac
+     on be.event_type=oac.event_type and be.occurences>oac.avg_occurences
+     group by business_id
+   having  count(distinct be.event_type)>1
 )
-
-select  distinct be.business_id from  
-playground.business_events be inner join event_cnt_cte ece
-  on be.business_id=ece.business_id
-inner join occerence_avg_cte oac
-  on be.event_type=oac.event_type
-where be.occurences>avg_occurences
+  select business_id from event_cnt_cte
